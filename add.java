b1291db0d22da4d0235c082773d1ed48c7f480cc
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-07 23:44:29 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class add{
+    public static void main(String[] args) {
+   int a,b,c;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter First number :");
+   a=sc.nextInt();
+   System.out.println("Enter Second number :");
+   b=sc.nextInt();
+   c=a+b;
+   System.out.println("Sum is:"+c);
+   System.out.println("DONE.");
+}
+}
