a150f9d633b75296ce346caa0128245a30d4e060
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-08 11:33:36 +0530

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String args[]){
+        System.out.print("Hello, Everyone\n");
+        System.out.print("My Name is Abhishek Verma\n");
+        System.out.print("I am from Pilibhit\n");
+        System.out.print("Currently I am pursuing B.tech cse\n");
+        System.out.print("Learning JAVA Language");
+    }
+}
