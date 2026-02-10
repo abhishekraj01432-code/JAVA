f669b4becca01fe19f410d0b3c2112a6a0cb01a2
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-10 09:22:29 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
 class Test{
-    public static void main(String args[]){
-        System.out.print("Hello, JAVA\n");
-        System.out.print("CSE 12 JAVA First Lab\n");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
-    }
+}
