cf80aae91540abd17a9169f92d5bea0366800854
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-18 21:46:21 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
-class Student {
-    String rollNo;
-    String name;
-    int age;
-    Student(String r, String n, int a) {
-        rollNo = r;
-        name = n;
-        age = a;
-    }
-    void display() {
-        System.out.println("Roll No: " + rollNo);
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("This is done by ABHISHEK VERMA, 2400320100055\n");
-        System.out.print("Enter roll number: ");
-        String r = sc.nextLine();  
-        System.out.print("Enter name: ");
-        String n = sc.nextLine();
-        System.out.print("Enter age: ");
-        int a = sc.nextInt();
-        Student s1 = new Student(r, n, a);
-        s1.display();
+
+public class Student {
+    public static void main(String[] args) {
+        try {
+            int marks, roll;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter roll number: ");
+            roll = scan.nextInt();
+            if (roll < 0)
+                throw new Exception("Roll number should be larger than zero");
+            System.out.print("Enter percentage of marks: ");
+            marks = scan.nextInt();
+            if (marks > 100 || marks < 0)
+                throw new Exception("percentage should be between 1-100");
+
+            System.out.println("Roll :" + roll);
+            System.out.println("Percentage of Marks: " + marks);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
